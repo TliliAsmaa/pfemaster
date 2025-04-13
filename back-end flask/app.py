5d39a091292cb52184e96732e7a2b1581a934ee9
TliliAsmaa <tliliasmaa12@gmail.com>
2025-04-13 20:17:11 +0100

    render deployment

--- back-end flask/app.py
+++ back-end flask/app.py
@@ -49,4 +49,6 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+      port = int(os.environ.get('PORT', 5000))  # Render fournit un PORT
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
