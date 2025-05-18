fd5be8ec2da80c51f8938e8972e466020c2073d5
BenKenza <155632022+BenKenza@users.noreply.github.com>
2025-05-18 21:44:50 +0100

    Update historypage.dart

--- pfe/lib/historypage.dart
+++ pfe/lib/historypage.dart
@@ -9,89 +9,13 @@ class HistoryPage extends StatefulWidget {
 
 class _HistoryPageState extends State<HistoryPage> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  late Stream<QuerySnapshot> _historyStream;
-  String? userId; // Variable pour stocker l'UID de l'utilisateur connecté
+  String? userId;
 
   @override
   void initState() {
     super.initState();
-    print("User ID: $userId");
-    // On récupère l'ID de l'utilisateur connecté, ici on suppose qu'il est déjà authentifié
     userId = FirebaseAuth.instance.currentUser?.uid;
-    if (userId != null) {
-      // Récupérer les prédictions de l'utilisateur spécifique
-      _historyStream = _firestore
-          .collection('users')
-          .doc(userId)
-          .collection('predictions')
-          .snapshots();
-    }
   }
-
-  // Fonction pour afficher les détails d'une prédiction
-  void _showDetails(BuildContext context, Map<String, dynamic> predictionData) {
-  String details = '''
-Âge : ${predictionData['age']}
-Sexe : ${predictionData['sex']== 1 ? 'Homme' : 'Femme'}
-Anémie : ${predictionData['anaemia'] == 1 ? 'Oui' : 'Non'}
-Diabète : ${predictionData['diabetes'] == 1 ? 'Oui' : 'Non'}
-Hypertension : ${predictionData['high_blood_pressure'] == 1 ? 'Oui' : 'Non'}
-Fumeur : ${predictionData['smoking'] == 1 ? 'Oui' : 'Non'}
-Créatinine phosphokinase : ${predictionData['creatinine_phosphokinase']}
-Fraction d’éjection : ${predictionData['ejection_fraction']}%
-Plaquettes : ${predictionData['platelets']}
-Créatinine sérique : ${predictionData['serum_creatinine']}
-Sodium sérique : ${predictionData['serum_sodium']}
-Durée de suivi (jours) : ${predictionData['time']}
-''';
-
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        
-        title: Container(
-          child: Column(
-            children: [
-              Text("Détails de la Prédiction" ,
-               style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                  letterSpacing: 1.2 // Couleur de texte modernisée
-                
-               ),),
-              Divider(
-                            color: Colors.grey, // Ligne de séparation supplémentaire
-                            thickness: 1.8,
-                          ),
-            ],
-          ),
-           
-          ),
-       
-        content: SingleChildScrollView(
-          child: Text(details, style: TextStyle(fontSize: 16)),
-        ),
-        actions: [
-          TextButton(
-            
-            style: TextButton.styleFrom(
-              
-              backgroundColor: Color(0xFFF7FBFF),
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Color(0xFFF7FBFF), width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            onPressed: () => Navigator.of(context).pop(),
-            child: Text('Fermer',style: TextStyle(color: Color(0xFF4A90E2),fontSize: 16),),
-          ),
-        ],
-      );
-    },
-  );
-}
 
   // Fonction pour formater la date
   String _formatDate(Timestamp timestamp) {
@@ -99,119 +23,256 @@ Durée de suivi (jours) : ${predictionData['time']}
     return "${dateTime.day}/${dateTime.month}/${dateTime.year} à ${dateTime.hour}:${dateTime.minute}";
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:  Color(0xFFF7FBFF),
-      appBar: AppBar(
-      backgroundColor:  Color(0xFFF7FBFF),
-        // Couleur d'AppBar modernisée
-      ),
-      body: userId == null
-          ? Center(child: Text("Utilisateur non connecté"))
-          : StreamBuilder<QuerySnapshot>(
-              stream: _historyStream,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.data!.docs.isEmpty) {
-                  return Center(child: Text("Aucune prédiction trouvée.",style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),));
-                }
+  // Fonction pour afficher les détails
+  void _showDetails(BuildContext context, String details) {
+    showDialog(
+      context: context,
+      builder:
+          (_) => AlertDialog(
+            title: Text("Détails"),
+            content: Text(details),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("Fermer"),
+              ),
+            ],
+          ),
+    );
+  }
 
-                final predictions = snapshot.data!.docs;
-                
-                return ListView.builder(
-                  
-                  padding: EdgeInsets.all(16),
-                  itemCount: predictions.length,
-                  itemBuilder: (context, index) {
-                    final prediction = predictions[index];
-                    final result = prediction['result'];
-                    final timestamp = prediction['timestamp'];
-                    final uid = prediction['uid'];
+  Widget _buildPredictionHistory() {
+    if (userId == null) return Center(child: Text("Utilisateur non connecté"));
 
-                    // Formater la date
-                    final formattedDate = _formatDate(timestamp);
+    return StreamBuilder<QuerySnapshot>(
+      stream:
+          _firestore
+              .collection('users')
+              .doc(userId)
+              .collection('predictions')
+              .orderBy('timestamp', descending: true)
+              .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
 
-                    // Déterminer la couleur et l'icône selon le résultat
-                    Color resultColor;
-                    IconData resultIcon;
-                    if (result == 'Risque élevé') {
-                      resultColor = Colors.red;
-                      resultIcon = Icons.error;
-                    } else if (result == 'Normal') {
-                      resultColor = Colors.green;
-                      resultIcon = Icons.check_circle;
-                    } else {
-                      resultColor = Colors.orange;
-                      resultIcon = Icons.warning;
-                    }
+        final predictions = snapshot.data!.docs;
+
+        return ListView.builder(
+          padding: EdgeInsets.all(16),
+          itemCount: predictions.length,
+          itemBuilder: (context, index) {
+            final prediction = predictions[index];
+            final result = prediction['result'];
+            final timestamp = prediction['timestamp'];
+
+            final formattedDate = _formatDate(timestamp);
+
+            Color resultColor;
+            IconData resultIcon;
+            if (result == 'Risque élevé') {
+              resultColor = Colors.red;
+              resultIcon = Icons.error;
+            } else if (result == 'Normal') {
+              resultColor = Colors.green;
+              resultIcon = Icons.check_circle;
+            } else {
+              resultColor = Colors.orange;
+              resultIcon = Icons.warning;
+            }
+
+            return Card(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              elevation: 4,
+              child: Column(
+                children: [
+                  ListTile(
+                    contentPadding: EdgeInsets.all(16),
+                    title: Text(
+                      "Prédiction ${index + 1}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Résultat : $result',
+                      style: TextStyle(fontSize: 16, color: resultColor),
+                    ),
+                    trailing: Icon(resultIcon, color: resultColor),
+                    onTap: () {
+                      _showDetails(
+                        context,
+                        "Détails supplémentaires à venir...",
+                      );
+                    },
+                  ),
+                  Divider(color: Colors.grey.shade300, thickness: 1.5),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.calendar_today, color: Colors.grey),
+                        SizedBox(width: 8),
+                        Text(
+                          'Date : $formattedDate',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildAnalysisHistory() {
+    if (userId == null) return Center(child: Text("Utilisateur non connecté"));
+
+    return StreamBuilder<QuerySnapshot>(
+      stream:
+          _firestore
+              .collection('users')
+              .doc(userId)
+              .collection('analyses')
+              .orderBy('timestamp', descending: true)
+              .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
+
+        final analyses = snapshot.data!.docs;
+
+        if (analyses.isEmpty)
+          return Center(child: Text("Aucune analyse trouvée"));
+
+        return ListView(
+          padding: EdgeInsets.all(16),
+          children:
+              analyses.map((analyseDoc) {
+                final Timestamp timestamp = analyseDoc['timestamp'];
+                final formattedDate = _formatDate(timestamp);
+
+                // Pour chaque analyse, on va charger ses résultats (sous-collection 'resultats')
+                return FutureBuilder<QuerySnapshot>(
+                  future:
+                      _firestore
+                          .collection('users')
+                          .doc(userId)
+                          .collection('analyses')
+                          .doc(analyseDoc.id)
+                          .collection('resultats')
+                          .get(),
+                  builder: (context, snapshotResultats) {
+                    if (!snapshotResultats.hasData)
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Center(child: CircularProgressIndicator()),
+                      );
+
+                    final resultats = snapshotResultats.data!.docs;
 
                     return Card(
-                      color: Colors.white,
-                      margin: EdgeInsets.symmetric(vertical: 8),
+                      margin: EdgeInsets.symmetric(vertical: 10),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
+                        borderRadius: BorderRadius.circular(12),
                       ),
                       elevation: 4,
-                      child: Column(
-                        children: [
-                          ListTile(
-                            
-                            contentPadding: EdgeInsets.all(16),
-                            title: Text(
-                              "prédiction $index",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                            subtitle: Text(
-                              'Résultat : $result',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: resultColor,
-                              ),
-                            ),
-                            trailing: Icon(
-                              resultIcon,
-                              color: resultColor,
-                            ),
-                            onTap: () {
-                              // Afficher les détails de la prédiction ici
-                              _showDetails(context, prediction.data() as Map<String, dynamic>);
-                            },
-                          ),
-                          Divider(
-                            color: Colors.grey.shade300, // Ligne de séparation entre les infos
-                            thickness: 1.5,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                            child: Row(
-                              children: [
-                                Icon(Icons.calendar_today, color: Colors.grey),
-                                SizedBox(width: 8),
-                                Text(
-                                  'Date : $formattedDate',
-                                  style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                      child: ExpansionTile(
+                        title: Text(
+                          "Analyse du $formattedDate",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        children:
+                            resultats.map((resultatDoc) {
+                              final identifiant = resultatDoc['identifiant'];
+                              final valeur = resultatDoc['valeur'].toString();
+                              final unite = resultatDoc['unite'];
+                              final interpretation =
+                                  resultatDoc['interpretation'];
+
+                              Color interpretationColor;
+                              if (interpretation.toLowerCase() == 'normal') {
+                                interpretationColor = Colors.green;
+                              } else if (interpretation.toLowerCase() ==
+                                      'élevé' ||
+                                  interpretation.toLowerCase() == 'haut') {
+                                interpretationColor = Colors.red;
+                              } else {
+                                interpretationColor = Colors.orange;
+                              }
+
+                              return ListTile(
+                                leading: Icon(
+                                  Icons.bloodtype,
+                                  color: interpretationColor,
                                 ),
-                              ],
-                            ),
-                          ),
-                          Divider(
-                            color: Colors.grey.shade300, // Ligne de séparation supplémentaire
-                            thickness: 1.5,
-                          ),
-                        ],
+                                title: Text(
+                                  identifiant,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Valeur : $valeur $unite'),
+                                    Text(
+                                      'Interprétation : $interpretation',
+                                      style: TextStyle(
+                                        color: interpretationColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                onTap: () {
+                                  _showDetails(
+                                    context,
+                                    "Test : $identifiant\nValeur : $valeur $unite\nInterprétation : $interpretation\nDate : $formattedDate",
+                                  );
+                                },
+                              );
+                            }).toList(),
                       ),
                     );
                   },
                 );
-              },
-            ),
+              }).toList(),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Historique'),
+          backgroundColor: Color(0xFF5C6BC0),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "Prédictions", icon: Icon(Icons.analytics)),
+              Tab(text: "Analyses", icon: Icon(Icons.medical_services)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [_buildPredictionHistory(), _buildAnalysisHistory()],
+        ),
+      ),
     );
   }
 }
-
