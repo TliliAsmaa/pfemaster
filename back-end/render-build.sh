f111154b57697c9ce7f4ab171c1a366a545baa50
TliliAsmaa <tliliasmaa12@gmail.com>
2025-05-31 11:56:07 +0100

    pour render

--- back-end/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-# Installer Tesseract OCR et ses dépendances
-apt-get update
-apt-get install -y tesseract-ocr libtesseract-dev libleptonica-dev
-
-# Installer les dépendances Python
-pip install -r requirements.txt
