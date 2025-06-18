b968967593d8432147911689fe021d9347dade77
BenKenza <155632022+BenKenza@users.noreply.github.com>
2025-06-18 15:35:29 +0100

    Update app.py

--- back-end/app.py
+++ back-end/app.py
@@ -211,7 +211,7 @@ def analyse():
         return jsonify({'error': 'Erreur interne', 'details': str(e)}), 500
     """
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
 
